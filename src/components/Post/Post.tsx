90d1ff57077c02fb15ed0c8cc1a52a82d8a80a92
Poghos Barseghyan <bpoghos94@gmail.com>
2024-01-15 01:13:55 +0400

    delete and post api requests

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,10 +1,15 @@
 import { useNavigate } from 'react-router'
 import { PostData } from './Post.interface'
 import './style.css'
+import { useState } from 'react'
+import Loading from '../Loading/Loading'
 
 
-const Post = ({ post }: { post: PostData }) => {
 
+
+const Post = ({ post, posts, setPosts }: { post: PostData, posts: PostData[], setPosts: any }) => {
+
+    const [loading, setLoading] = useState<boolean>(false)
 
     const navigate = useNavigate()
 
@@ -12,23 +17,43 @@ const Post = ({ post }: { post: PostData }) => {
         navigate(`/posts/${id}`)
     }
 
-    // const deletePost = async (id: number) => {
-    //     const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-    //         method: 'DELETE',
-    //         headers: {
-    //             'Content-type': 'application/json'
-    //         }
-    //     })
 
-    // }
+    const deletePost = async (id: number | string) => {
+        setLoading(true)
+        try {
+            await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+                method: 'DELETE'
+            })
+
+
+            let filtered = posts.filter((post) => post.id !== id)
+            setPosts(filtered)
+
+        } catch (error) {
+            setLoading(false)
+            throw error
+        }
+        finally {
+            setLoading(false)
+        }
+
+    }
 
 
 
     return (
-        <div className="post" onClick={() => handleClick(post.id)}>
-            <h1>{post.title}</h1>
-            <button>Delete</button>
-        </div>
+
+        <>
+            {
+                loading ? <Loading /> : (
+                    <div className="post" onClick={() => handleClick(post.id)}>
+                        <h1>{post.title}</h1>
+                        <button onClick={(e) => { e.stopPropagation(); deletePost(post.id) }}>Delete</button>
+                    </div>
+                )
+            }
+
+        </>
     )
 }
 
--- src/pages/PostsPage/PostsPage.tsx
+++ src/pages/PostsPage/PostsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import Post from "../../components/Post/Post"
 
 import './style.css'
@@ -10,6 +10,7 @@ const PostsPage = () => {
     const [posts, setPosts] = useState<PostData[]>([])
     const [loading, setLoading] = useState<boolean>(false)
     const [error, setError] = useState<string>('')
+    const [title, setTitle] = useState<string>('')
 
 
     const getPosts = async () => {
@@ -29,17 +30,49 @@ const PostsPage = () => {
         getPosts()
     }, [])
 
+    console.log(posts);
 
+    const handleTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value)
+    }
+
+
+    const handleAddPost = async () => {
+        try {
+            const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    title,
+                    body: 'Your Body',
+                    userId: 1
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            setPosts([data, ...posts])
+        } catch (error) {
+            console.error('Error during post creation:', error);
+        }
+    };
 
 
 
     return (
         <div className="posts">
+            <input type="text" placeholder="enter your title..." onChange={handleTitle} value={title} />
+            <button onClick={handleAddPost}>add post</button>
             {
                 error ||
                     loading ? <Loading /> :
                     posts.map((post: PostData) => {
-                        return <Post key={post.id} post={post} />
+                        return <Post key={post.id} post={post} posts={posts} setPosts={setPosts} />
                     })
             }
         </div>
